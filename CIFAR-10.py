7d9796f4afd11e3c26e9498b56ddba4fc7aecabb
yhdm44 <1453305264@qq.com>
2026-05-17 21:38:11 +0800

    解决报错问题,准确率提升到87.54%

--- CIFAR-10.py
+++ CIFAR-10.py
@@ -65,7 +65,7 @@ class CNN(nn.Module):
     
 model = CNN().to(device)
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)  
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-4)  
 # 训练模型  
 num_epochs = 50
 for epoch in range(num_epochs):
@@ -87,7 +87,7 @@ with torch.no_grad():
     for images, labels in test_loader:
         images, labels = images.to(device), labels.to(device)
         outputs = model(images)
-        _, predicted = outputs.argmax(dim=1)
+        predicted = outputs.argmax(dim=1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
     accuracy = 100 * correct / total
